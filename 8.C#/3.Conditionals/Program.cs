5f50d4edd40d130592e28e5e06b1d7ae474bd45c
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-05 12:26:42 -0500

    Ejercicios basicos C#

---
+++ 8.C#/3.Conditionals/Program.cs
@@ -0,0 +1,26 @@
+﻿bool isAuthenticated = false;
+
+//If
+if (isAuthenticated)
+Console.WriteLine("Bienvenido al sistema");
+else
+Console.WriteLine("Logeo incorrecto");
+
+//Ternario
+string message = isAuthenticated ? "Bienvenido al sistema" : "Logeo incorrecto (ternario)";
+Console.WriteLine(message);
+
+//Switch
+string usuario = "admin";
+switch(usuario)
+{
+    case "admin":
+       Console.WriteLine("Es admin");
+       break;
+    case "usuarioNormal":
+       Console.WriteLine("Es usuario");
+       break;
+    default:
+       Console.WriteLine("Usuario no válido");
+       break;
+}
---
+++ 8.C#/4.Bucles/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] names = new string [] {"Daniel Jay", "Yuliana Gonzalez", "Yeimy Corzo", "Sirly Esteban", "Mariana Peña"};
+//FOR
+Console.WriteLine("-------------------------------------------------------------");
+for(int i=0; i<names.Length; i++){
+    Console.WriteLine(names[i]);
+} 
+
+//FOREACH
+Console.WriteLine("-------------------------------------------------------------");
+foreach(string name in names){
+    Console.WriteLine(name);
+}
+//WHILE
+Console.WriteLine("-------------------------------------------------------------");
+int contador = 0;
+while(contador < names.Length) {
+    Console.WriteLine(names[contador]);
+    contador++;
+}
+//DO WHILE
+Console.WriteLine("-------------------------------------------------------------");
+int contador2 = 0;
+do {
+    Console.WriteLine(names[contador2]);
+    contador2++;
+}while(contador2 < names.Length);
