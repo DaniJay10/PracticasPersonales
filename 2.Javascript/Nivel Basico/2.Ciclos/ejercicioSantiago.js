d8bf3b5520e23fe2129fa159058b218d4ca5a063
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-06 16:46:04 -0500

    Archivos C

--- 2.Javascript/Nivel Basico/2.Ciclos/ejercicioSantiago.js
+++
@@ -1,6 +0,0 @@
-let primo = Number(prompt("Digite cantidad de primos que quiere ver"));
-let num = 0;
-for(let i=0;i<=primo;i++){
-    if (i%2 == 1)
-    alert(i)
-}
--- 2.Javascript/Nivel Basico/2.Ciclos/taller/ejercicio1.js
+++
@@ -1,6 +0,0 @@
-let num= Number(prompt("Ingrese un numero"));
-for(let i=0;i<=num;i++){
-	if(i%2 == 1){
-		alert(i)
-	}
-}
