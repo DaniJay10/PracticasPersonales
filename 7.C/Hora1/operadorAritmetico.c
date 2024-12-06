af80f0e1694d8b5ea0958397670c322d60b8caca
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-06 16:45:02 -0500

    Archivos PHP

--- 7.C/Hora1/operadorAritmetico.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int numero = 5;
-    numero++;//incrementar un numero
-    numero++;
-    numero--;//disminuir un numero
-    printf("%d", numero);
-    return 0;
-}
