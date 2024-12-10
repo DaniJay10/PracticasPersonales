620fde52598d6992c95937c07012ddeee9e080eb
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-10 18:56:57 -0500

    recuperacion carpeta 16 y 17

---
+++ 8.C#/17.Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using coreAdvancedConcepts;
+
+CalculateDelegate c1 = new CalculateDelegate(DelegateExample.addition);
+
+CalculateDelegate c2 = new CalculateDelegate(DelegateExample.subtraction);
+
+
+CalculateDelegate c3 = new CalculateDelegate(DelegateExample.multiplication);
+
+c1(100);
+Console.WriteLine(DelegateExample.getNumber());
+c2(77);
+Console.WriteLine(DelegateExample.getNumber());
+c3(2);
+Console.WriteLine(DelegateExample.getNumber());
+
+
+//Delegate multicast : Cada delegate puede apuntar a mas de 1 metodo.
+c1 += new CalculateDelegate(DelegateExample.multiplication);
+c1(9);
+Console.WriteLine(DelegateExample.getNumber());
