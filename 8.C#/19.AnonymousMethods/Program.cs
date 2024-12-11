569b2add1e72c92ebfaf34488a8cb2b9badbd9dd
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-10 19:00:39 -0500

    C# 10/12/2024

---
+++ 8.C#/19.AnonymousMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConceptosBasicos;
+
+Codigo codigo = new Codigo();
+
+// Asignar un método anónimo al delegado
+Codigo.DelegadoMensaje mensaje = delegate
+{
+    Console.WriteLine("¡Este es un método anónimo!");
+};
+
+// Llamar al método que ejecuta el delegado
+codigo.EjecutarMetodo(mensaje);
+
+Console.ReadLine();
+
---
+++ 8.C#/20.Lambda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public delegate void MiDelegado(string mensaje);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MiDelegado mostrarMensaje = (mensaje) => {
+            Console.WriteLine($"El mensaje es: {mensaje}");
+        };
+
+        mostrarMensaje("¡Hola desde una expresión lambda!");
+
+        Console.ReadLine();
+    }
+}
