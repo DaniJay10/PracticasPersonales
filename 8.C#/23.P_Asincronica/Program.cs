092a757154f4f12d9835ddbac54df04ece444f2b
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-13 18:46:50 -0500

    p. sincronica/asincronica

---
+++ 8.C#/23.P_Asincronica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program {
+    static async Task Main(){
+        Console.WriteLine("Iniciando tareas asincrónicas...");
+        // Iniciamos ambas tareas asincrónicamente
+        Task tareaCafe = PrepararCafe();
+        Task tareaTostadas = HacerTostadas();
+        
+        await Task.WhenAll(tareaCafe, tareaTostadas);
+
+        Console.WriteLine("¡Todo listo!");
+    }
+    static async Task PrepararCafe()
+    {
+        Console.WriteLine("Preparando el café...");
+        await Task.Delay(3000); 
+        Console.WriteLine("Café listo.");
+    }
+    static async Task HacerTostadas()
+    {
+        Console.WriteLine("Haciendo las tostadas...");
+        await Task.Delay(2000); 
+        Console.WriteLine("Tostadas listas.");
+    }
+
+}
