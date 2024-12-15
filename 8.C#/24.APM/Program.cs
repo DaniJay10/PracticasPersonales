d4e8d65f88481e3bb0711e9fa5814548e870db66
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-15 18:55:24 -0500

    Patrones programacion asincronica

---
+++ 8.C#/24.APM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Iniciando operación de suma asíncrona...");
+
+        // Ejecutamos la suma de manera asíncrona usando Task
+        Task<int> tareaSuma = Task.Run(() => Sumar(5, 3));
+
+        Console.WriteLine("Haciendo otras cosas mientras se realiza la suma...");
+
+        // Esperamos el resultado de la tarea
+        int resultado = await tareaSuma;
+
+        Console.WriteLine($"La suma ha terminado. Resultado: {resultado}");
+    }
+
+    // Método que realiza la suma
+    static int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+}
