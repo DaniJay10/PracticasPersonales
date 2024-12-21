0b3d9abfc4b1e9747e46533283bdce0934c570c1
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-20 19:30:05 -0500

    Ejercicio 11

---
+++ 8.C#/27.EJERCICIOS/ejercicio11/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ejercicio11{
+    class Program{
+    enum DiasSemanas : int {
+        Lunes,
+        Martes,
+        Miercoles,
+        Jueves,
+        Viernes,
+        Sabado,
+        Domingo
+    }
+        static void Main(string[] args){
+            int dia = 0;
+            Console.WriteLine("Ingrese numero dia de la semana");
+            dia = int.Parse(Console.ReadLine());
+            while ( dia > 7 || dia <= 0){
+                Console.WriteLine("Error: Ingrese un dia valido (1-7)");
+                dia = int.Parse(Console.ReadLine());
+            }
+            DiasSemanas diaSemana = (DiasSemanas)(dia - 1);
+            Console.WriteLine($"El día de la semana es: {diaSemana}");
+
+
+        
+        }
+    }
+}
