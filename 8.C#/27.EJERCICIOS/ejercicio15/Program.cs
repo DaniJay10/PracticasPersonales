abda79572d557f2ee5ff604f0a4684f36ec800ec
DaniJay10 <danielpinzon20jay@gmail.com>
2024-12-23 15:45:22 -0500

    Ejercicios 15 y 16

---
+++ 8.C#/27.EJERCICIOS/ejercicio15/Program.cs
@@ -0,0 +1,6 @@
+﻿using coreObjectOrientedConcepts;
+
+string cadena = "hola mundo de locos";
+int resultado = cadena.ContadorPlabras();
+Console.WriteLine($"Cadena: {cadena}");
+Console.WriteLine($"Numero palabras oracion: {resultado}");
