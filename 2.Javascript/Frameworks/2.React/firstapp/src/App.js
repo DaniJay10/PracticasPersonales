422bd39a0a18b1cec325d2f788e03b07e2bcaf3a
DaniJay10 <danielpinzon20jay@gmail.com>
2025-01-13 16:42:28 -0500

    Primer componente react

--- 2.Javascript/Frameworks/2.React/firstapp/src/App.js
+++ 2.Javascript/Frameworks/2.React/firstapp/src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { PrimerComponente } from './components/PrimerComponente';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Primer proyecto react</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,7 +14,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> 
+        <PrimerComponente />
+        <PrimerComponente />
       </header>
     </div>
   );
