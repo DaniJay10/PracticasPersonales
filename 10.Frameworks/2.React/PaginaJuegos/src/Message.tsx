54d8b8980a5f3826d3f2ce910807a7812080003c
DaniJay10 <danielpinzon20jay@gmail.com>
2025-01-20 17:37:09 -0500

    Proyecto1 react

--- 10.Frameworks/2.React/PaginaJuegos/src/Message.tsx
+++
@@ -1,8 +0,0 @@
-//PascalCasing
-function Message(){
-    const name = "Daniel";
-    if (name)
-        return <h1>Hello {name}!</h1>;
-    return <h1>Hello whitout name</h1>;
-}
-export default Message;
