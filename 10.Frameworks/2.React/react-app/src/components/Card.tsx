3bce1ecf84c1b532c625ff76c0f32ab1aeae7de7
DaniJay10 <danielpinzon20jay@gmail.com>
2025-01-20 22:11:16 -0500

    Props and children 20/01/25

---
+++ 10.Frameworks/2.React/react-app/src/components/Card.tsx
@@ -0,0 +1,15 @@
+import CardBody from "./CardBody";
+import { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+}
+function Card(props: Props) {
+  const { children } = props;
+  return (
+    <div className="card" style={{ width: "350px" }}>
+      <div className="card-body">{children}</div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ 10.Frameworks/2.React/react-app/src/components/CardBody.tsx
@@ -0,0 +1,16 @@
+interface CardBodyProps {
+  title: String;
+  text: string;
+}
+
+function CardBody(props: CardBodyProps) {
+  const { title, text } = props;
+  return (
+    <>
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
+    </>
+  );
+}
+
+export default CardBody;
