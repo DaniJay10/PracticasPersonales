d7a5fd7edae339669a7410bcbb089686e00ebb40
DaniJay10 <danielpinzon20jay@gmail.com>
2025-01-24 15:38:14 -0500

    funciones como props

--- 10.Frameworks/2.React/react-app/src/App.tsx
+++ 10.Frameworks/2.React/react-app/src/App.tsx
@@ -4,12 +4,19 @@ import List from "./components/List";
 
 function App() {
   const list = ["Messi", "Iniesta", "Xavi"];
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+  const handleSelect2 = (elemento: string) => {
+    console.log("mostrando", elemento);
+  };
   return (
     <>
       <Card>
         <CardBody title="Esto es el título" text="Este es el texto" />
       </Card>
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
     </>
   );
 }
